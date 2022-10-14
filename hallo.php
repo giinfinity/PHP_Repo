16bb1d183557be816d774d298e5ed9a5490a7019
Gina Meier <ginam.meier.m@gmail.com>
2022-10-14 08:36:28 +0200

    first commit

---
+++ hallo.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>100 Zahlen</title>
+</head>
+<body>
+<?php
+//$zahlen = array();
+#$Container = array();
+#for ($i = 1; $i <= 99; ++$i) {
+  #$zahlen[] = $i;
+#}
+#var_dump($zahlen);
+#var_dump($i);
+?>
+<p>Grid mit 100 Zahlen</p>
+<?php
+echo '<table style="border:1px solid black">';
+  $zahlen = array();
+  for ($i = 1; $i <= 9; ++$i) {
+    $zahlen[] = $i;
+    echo 
+    "<td style=\"border:1px solid black;\">
+     <span style=\"color:blue;\">".$i."</span></td>";
+     for ($i = 10; $i <=18; ++$i) {
+      $zahlen[] = $i;
+      echo 
+      "<tr><td style=\"border:1px solid black;\">
+       <span style=\"color:blue;\">".$i."</span></tr></td>";
+    }
+  }
+  for ($i = 10; $i <= 18; ++$i) {
+    $zahlen[] = $i;
+    echo 
+    "<tr><td style=\"border:1px solid black;\">
+     <span style=\"color:blue;\">".$i."</span></tr>";
+     for ($i = 28; $i <= 35; ++$i) {
+      $zahlen[] = $i;
+      echo 
+      "<tr style=\"border:1px solid black;\">
+       <span style=\"color:blue;\">".$i."</span></tr>";
+    }
+  }
+  echo '</table>'; ?>
+</body>
+</html>
